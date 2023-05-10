aedbecc9db567cb7c91c307dbf43b8a9bebe7977
khiemld <khiemld.0204@gmail.com>
2023-05-10 23:21:05 +0700

    Merge main-feature

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import App from "./app";
 import { validateEnv } from '@core/utils';
 import UserRoute from '@modules/users/user.route';
 import AuthRoute from '@modules/auth/auth.route';
+import { PlanRoute } from '@modules/plan';
+import { ListRoute } from '@modules/listTask';
 
 
 validateEnv();
@@ -11,7 +13,9 @@ validateEnv();
 const routes = [
     new IndexRoute(),
     new UserRoute(),
-    new AuthRoute()]
+    new AuthRoute(),
+    new PlanRoute(),
+    new ListRoute()]
     
 const app = new App(routes);
 
